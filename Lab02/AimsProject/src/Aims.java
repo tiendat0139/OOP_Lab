fb21f1cafc5ecb12c7cdb4a99208249417d0c772
tiendat0139 <dattien090909@gmail.com>
2022-11-09 23:35:09 +0700

    Create Aism Project

---
+++ Lab02/AimsProject/src/Aims.java
@@ -0,0 +1,40 @@
+public class Aims {
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+        DigitalVideoDisc dvd1= new DigitalVideoDisc("The Lion King", "Animation",
+                "Roger Aller",87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction",
+                "Gerge Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.89f);
+
+        // Add DVD to cart
+        anOrder.addDigitalVideoDisc(dvd1);
+        anOrder.addDigitalVideoDisc(dvd2);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        // Display cart
+        System.out.println();
+        anOrder.displayCart();
+
+        // Display total cost
+        System.out.printf("Total Cost: %.2f \n", anOrder.totalCost());
+
+        // Sort cart by title
+        anOrder.sortCartByTitle();
+        System.out.println();
+        System.out.println("--- Sort cart by title ---");
+        anOrder.displayCart();
+
+        // Sort cart by title
+        anOrder.sortCartByCost();
+        System.out.println();
+        System.out.println("--- Sort cart by Cost ---");
+        anOrder.displayCart();
+
+        // Remove dist from cart
+        System.out.println();
+        System.out.println("-- Remove " + dvd2.getTitle() + "from cart --");
+        anOrder.removeDigitalVideoDisc(dvd2);
+        anOrder.displayCart();
+    }
+}
---
+++ Lab02/AimsProject/src/Cart.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private int qtyOrdered;
+    private ArrayList<DigitalVideoDisc> itemsOrdered = new ArrayList<DigitalVideoDisc>();
+
+    public int getQtyOrder() {
+        return qtyOrdered;
+    }
+
+    public void setQtyOrder(int qtyOrder) {
+        this.qtyOrdered = qtyOrder;
+    }
+
+    public boolean addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered <= MAX_NUMBERS_ORDERED){
+            itemsOrdered.add(disc);
+            qtyOrdered++;
+            System.out.println("Add DVD into cart successfully!");
+            return true;
+        } else {
+            System.out.println("The cart is almost full");
+            return false;
+        }
+    }
+
+    public boolean removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        boolean isFound = itemsOrdered.contains(disc);
+        if(isFound){
+            itemsOrdered.remove(disc);
+            qtyOrdered--;
+            System.out.println("Remove this DigitalVideoDisc from cart successfully!");
+            return true;
+        } else {
+            System.out.println("This DigitalVideoDisc not added to cart");
+            return false;
+        }
+    }
+
+    public void displayCart() {
+        System.out.println("------ Cart Detail ------");
+        for (DigitalVideoDisc disc : itemsOrdered){
+            System.out.printf("%-20s %-20s %-20s %-10d %-10.2f \n", disc.getTitle(), disc.getCategory(),
+                    disc.getDirector(), disc.getLength(), disc.getCost());
+        }
+    }
+
+    public double totalCost() {
+        double total = 0;
+        for (DigitalVideoDisc disc : itemsOrdered){
+            total += disc.getCost();
+        }
+        return total;
+    }
+
+    public void sortCartByTitle() {
+        Collections.sort(itemsOrdered, new Comparator<DigitalVideoDisc>() {
+            @Override
+            public int compare(DigitalVideoDisc o1, DigitalVideoDisc o2) {
+                String title1 = o1.getTitle();
+                String title2 = o2.getTitle();
+                int sComp = title1.compareTo(title2);
+
+                if (sComp != 0) {
+                    return sComp;
+                }
+
+                Double cost1 = o1.getCost();
+                Double cost2 = o2.getCost();
+                return cost1.compareTo(cost2);
+            }
+        });
+    }
+
+    public void sortCartByCost() {
+        Collections.sort(itemsOrdered, new Comparator<DigitalVideoDisc>() {
+            @Override
+            public int compare(DigitalVideoDisc o1, DigitalVideoDisc o2) {
+                Double cost1 = o1.getCost();
+                Double cost2 = o2.getCost();
+                int sComp = cost1.compareTo(cost2);
+
+                if (sComp != 0) {
+                    return sComp;
+                }
+                String title1 = o1.getTitle();
+                String title2 = o2.getTitle();
+                return title1.compareTo(title2);
+            }
+        });
+    }
+
+
+}
