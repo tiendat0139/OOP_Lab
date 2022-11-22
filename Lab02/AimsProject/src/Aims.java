c3c0495eb907e7604185b005160a6a233dd4f775
tiendat0139 <dattien090909@gmail.com>
2022-11-22 21:50:41 +0700

    Open the Cart class

--- Lab02/AimsProject/src/Aims.java
+++ Lab02/AimsProject/src/Aims.java
@@ -2,21 +2,21 @@ import java.sql.Array;
 
 public class Aims {
     public static void main(String[] args) {
-        Cart anOrder = new Cart();
         DigitalVideoDisc dvd1= new DigitalVideoDisc("The Lion King", "Animation",
                 "Roger Aller",87, 19.95f);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction",
                 "Gerge Lucas", 87, 24.95f);
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.89f);
 
+//        Cart anOrder = new Cart();
         // Add DVD to cart
-        anOrder.addDigitalVideoDisc(dvd1);
-        anOrder.addDigitalVideoDisc(dvd2);
-        anOrder.addDigitalVideoDisc(dvd3);
+//        anOrder.addDigitalVideoDisc(dvd1);
+//        anOrder.addDigitalVideoDisc(dvd2);
+//        anOrder.addDigitalVideoDisc(dvd3);
 
         // Display cart
-        System.out.println();
-        anOrder.displayCart();
+//        System.out.println();
+//        anOrder.displayCart();
 
 //        // Display total cost
 //        System.out.printf("Total Cost: %.2f \n", anOrder.totalCost());
@@ -44,5 +44,16 @@ public class Aims {
 //        anOrder.addDigitalVideoDisc(dvdList);
 //        anOrder.displayCart();
 
+        Cart cart = new Cart();
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+        cart.addDigitalVideoDisc(dvd3);
+
+        //Test the print method
+        cart.print();
+
+        //To-do: Test the search methods
+        cart.searchInCart(2);
+        cart.searchInCart("The Lion King");
     }
 }
