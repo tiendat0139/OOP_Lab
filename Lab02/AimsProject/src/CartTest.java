39e35e96642a8d6b775d147332c001a310b9c360
tiendat0139 <dattien090909@gmail.com>
2022-11-22 23:03:13 +0700

    Implement the Store class

--- Lab02/AimsProject/src/CartTest.java
+++ Lab02/AimsProject/src/CartTest.java
@@ -1,4 +1,4 @@
-public class ClassTest {
+public class CartTest {
     public static void main(String[] args) {
         DigitalVideoDisc dvd1= new DigitalVideoDisc("The Lion King", "Animation",
                 "Roger Aller",87, 19.95f);
