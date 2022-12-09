81d8f0133b15d46ca0d6522cef20ac44b70835ab
tiendat0139 <83073649+tiendat0139@users.noreply.github.com>
2022-12-09 15:56:00 +0700

    Revert "Complete application"

--- Lab02/AimsProject/src/hust/soict/dsai/aims/test/store/StoreTest.java
+++ Lab02/AimsProject/src/hust/soict/dsai/aims/test/store/StoreTest.java
@@ -1,16 +1,20 @@
 package hust.soict.dsai.aims.test.store;
 
-import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 import hust.soict.dsai.aims.store.Store;
 
 public class StoreTest {
     public static void main(String[] args) {
-        DigitalVideoDisc dvd1= new DigitalVideoDisc(1,"The Lion King", "Animation",
-                19.95f,87 , "Roger Aller");
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc(2,"Star Wars", "Science Fiction",
-                24.95f, 87, "Gerge Lucas");
+        DigitalVideoDisc dvd1= new DigitalVideoDisc("The Lion King", "Animation",
+                "Roger Aller",87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction",
+                "Gerge Lucas", 87, 24.95f);
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.89f);
         Store store = new Store();
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.addDVD(dvd3);
+        store.removeDVD(dvd2);
 
     }
 }
